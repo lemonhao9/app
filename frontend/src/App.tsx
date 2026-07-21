87733a61fdddad8447dfe15a45df744beb13de96
Lemon Hao <leohamon9@gmail.com>
2026-07-21 10:09:00 +0200

    feat : profil route protected

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
+import { ProtectedRoute } from './components/ProtectedRoute'
 import { LandingPage } from './pages/LandingPage'
 import { Forfaits } from './pages/Forfaits'
 import { About } from './pages/About'
 import { Reserver } from './pages/Reserver'
 import { Login } from './pages/Login'
 import { Signup } from './pages/Signup'
+import { Profil } from './pages/Profil'
 
 function App() {
   return (
@@ -13,6 +15,9 @@ function App() {
       <Route path="/forfaits" element={<Forfaits />} />
       <Route path="/about" element={<About />} />
       <Route path="/reserver" element={<Reserver />} />
+      <Route element={<ProtectedRoute />}>
+      <Route path="/profil" element={<Profil />} />
+      </Route>
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
     </Routes>
--- frontend/src/pages/Profil.tsx
+++ frontend/src/pages/Profil.tsx
@@ -4,23 +4,25 @@ import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 
 export function Profil() {
-  const { user, logout } = useAuth();
-  const navigate = useNavigate();
+    const { user, logout } = useAuth();
+    const navigate = useNavigate();
 
-  return (
-    <div>
-      <NavBar />
-      <h1>Profil</h1>
-      {user && (
-        <div>
-          <p>Nom: {user.name}</p>
-          <p>Email: {user.email}</p>
-          <p>Téléphone: {user.phone}</p>
-          <p>Adresse: {user.address?.label}</p>
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+            <NavBar dark/>
+            {user && (
+                <div className="p-6 rounded shadow-md w-80">
+                    <h1 className="text-center">Profil</h1>
+                    <p>Nom: {user.name}</p>
+                    <p>Email: {user.email}</p>
+                    <p>Téléphone: {user.phone}</p>
+                    <div className="flex flex-col gap-4 mt-4">
+                        <Button onClick={() => { navigate('/reserver') }}>Prendre un rendez-vous</Button>
+                        <Button onClick={logout} className="bg-red-900">Se déconnecter</Button>
+                    </div>
+                </div>
+            )}
+
         </div>
-      )}
-        <Button onClick={() => {navigate('/Reserver')}}>Prendre un rendez-vous</Button>
-        <Button onClick={logout}>Se déconnecter</Button>
-    </div>
-  );
+    );
 }
